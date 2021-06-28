856cdfec59e1110b0bed233e688740cd125b61e3
Hennadii Shpak <borchuga@gmail.com>
2021-06-28 08:22:17 +0300

    decorator

--- patterns/decorator.js
+++ patterns/decorator.js
@@ -13,21 +13,15 @@ class Macbook {
 let decorator = (() => {
   const memory = function (macbook) {
     const v = macbook.cost();
-    macbook.cost = () => {
-      v + 75;
-    };
+    macbook.cost = () => v + 75;
   };
   const engraving = function (macbook) {
     const v = macbook.cost();
-    macbook.cost = () => {
-      v + 200;
-    };
+    macbook.cost = () => v + 200;
   };
   const insurance = function (macbook) {
     const v = macbook.cost();
-    macbook.cost = () => {
-      v + 250;
-    };
+    macbook.cost = () => v + 250;
   };
   return {
     decorate(macbook) {
@@ -40,5 +34,57 @@ let decorator = (() => {
 
 let macbook = new Macbook();
 macbook.cost();
+console.log('macbook.cost()', macbook.cost());
 
 decorator.decorate(macbook);
+console.log('macbook.cost()', macbook.cost());
+
+class Car {
+  constructor() {
+    this.price = 10000;
+    this.model = 'Car';
+  }
+  getPrice() {
+    return this.price;
+  }
+  getDescription() {
+    return this.model;
+  }
+}
+
+class Tesla extends Car {
+  constructor() {
+    super();
+    this.price = 25000;
+    this.model = 'Tesla';
+  }
+}
+
+class Autopilot {
+  constructor(car) {
+    this.car = car;
+  }
+  getPrice() {
+    return this.car.getPrice() + 5000;
+  }
+  getDescription() {
+    return `${this.car.getDescription()} with autopilot`;
+  }
+}
+class Parctronic {
+  constructor(car) {
+    this.car = car;
+  }
+  getPrice() {
+    return this.car.getPrice() + 3000;
+  }
+  getDescription() {
+    return `${this.car.getDescription()} with parctronic`;
+  }
+}
+
+let tesla = new Tesla();
+tesla = new Autopilot(tesla);
+tesla = new Parctronic(tesla);
+console.log(tesla.getPrice());
+console.log(tesla.getDescription());
