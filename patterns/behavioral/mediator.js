768dcaa95af3e43adc793fc965c9e3761c5dbf51
Hennadii Shpak <borchuga@gmail.com>
2021-06-29 00:41:15 +0300

    mediator

--- patterns/behavioral/mediator.js
+++ patterns/behavioral/mediator.js
@@ -27,3 +27,41 @@ var mediator = (function () {
     },
   };
 })();
+
+class OfficialDealer {
+  constructor() {
+    this.customers = [];
+  }
+  orderAuto(customer, auto, info) {
+    const name = customer.getName();
+    console.log(`Order name: ${name}. Order auto is ${auto}`);
+    console.log(`Additional info: ${info}`);
+    this.addToCustomersList(name);
+  }
+  addToCustomersList(name) {
+    this.customers.push(name);
+  }
+  getCustomerList() {
+    return this.customers;
+  }
+}
+class Customer {
+  constructor(name, dealerMediator) {
+    this.name = name;
+    this.dealerMediator = dealerMediator;
+  }
+  getName() {
+    return this.name;
+  }
+  makeOrder(auto, info) {
+    this.dealerMediator.orderAuto(this, auto, info);
+  }
+}
+
+const mediators = new OfficialDealer();
+
+const a = new Customer('Jeka', mediators);
+const b = new Customer('Serega', mediators);
+
+a.makeOrder('Tesla', 'aaaaa');
+b.makeOrder('Moskvich', 'bbbbb');
